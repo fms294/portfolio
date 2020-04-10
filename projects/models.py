6c9cd73a9c4d15def60f84d8e67e6626eda0d3d6
Fantamady <fms294@nyu.edu>
2020-04-10 19:11:37 -0400

    added templates, source code link

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=100)
+    image = models.FilePathField(path='projects/static/img')
+    source_code = models.URLField(max_length=100, blank=True)
+
+    # def url_text(self):
+    #     parsed_url = urllib.parse(self.source_code)
+    #     return parsed_url
+
+    def __str__(self):
+        return self.title
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Project
+
 
 # Create your views here.
+
+
+def index(request):
+    projects = Project.objects.all()
+    context ={
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context=context)
+
+
+def detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context=context)
+
